32cc57f8bde88308dbd3d11cd3fd5620fbcfa6e3
otienotilen <otienotilen@gmail.com>
2023-06-24 17:01:29 +0300

    final layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,9 +29,10 @@ const Hero = () => {
         <div className='hero__image'>
           <Image src="/hero.png" alt="hero"
           fill className='object-contain'/>
+          </div>
           <div className='hero__image-overlay'/>
         </div>
-      </div>
+      
     </div>
   )
 } 
