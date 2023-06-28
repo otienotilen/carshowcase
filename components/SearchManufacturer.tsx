8263a3a159341ebcbd0b639bea7e78ca0b7f840d
otienotilen <otienotilen@gmail.com>
2023-06-28 12:15:20 +0300

    search

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,10 @@
+import { SearchManufacturerProps } from '@/types'
+import React from 'react'
+
+export default function SearchManufacturer({manufacturer, 
+  setManufacturer}: SearchManufacturerProps) {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
