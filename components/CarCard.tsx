e95648610dbde236e284b858c13dc1f5a9b18c5b
otienotilen <otienotilen@gmail.com>
2023-07-05 19:36:47 +0300

    Rapid API cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -8,10 +8,12 @@ import { calculateCarRent } from '@/utls';
 
 interface CarCardProps {
     car: CarProps;
+
 }
 
 const CarCard = ({ car } : CarCardProps) => {
     const {city_mpg, year, make, model, transmission, drive} = car;
+    const {IsOpen, setIsOpen} = useState(false);
     const carRent = calculateCarRent(city_mpg, year);
   return (
     <div className='car-card group'>
@@ -35,22 +37,48 @@ const CarCard = ({ car } : CarCardProps) => {
            className='object-contain'/>
         </div>
         <div className='relative flex w-full mt-2'>
-            <div className='flex group-hover: invisible w-full
+            <div className='flex group-hover:invisible w-full
             justify-between text-gray'>
                 <div className='flex flex-col justify-center
                 items-center gap-2'>
-                    <Image src="/steering-wheel.svg" width={20}
+                     <Image src="/steering-wheel.svg" width={20}
                     height={20} alt='steering wheel'/>
                     <p className='text-[14px]'>
                         {transmission === 'a' ? 'Automatic' : 
                         'Manual'}
                     </p>
                 </div>
+                <div className='flex flex-col justify-center
+                items-center gap-2'>
+                     <Image src="/tire.svg" width={20}
+                    height={20} alt='tire'/>
+                    <p className='text-[14px]'>
+                        {drive.toUpperCase()}
+                    </p>
+                </div>
+                <div className='flex flex-col justify-center
+                items-center gap-2'>
+                     <Image src="/gas.svg" width={20}
+                    height={20} alt='gas'/>
+                    <p className='text-[14px]'>
+                        {city_mpg} MPG
+                    </p>
+                </div>
 
             </div>
-
+               <div className='car-card__btn-container'>
+                <CustomButton
+                   title='View More'
+                   containerStyles='w-full py-[16px]
+                   rounded-full bg-primary-blue'
+                   textStyles='text-white text-[14px]
+                   leading-[17px] font-bold'
+                   rightIcon='/right-arrow.svg'
+                   handleClick={() => setIsOpen(true)}
+                 />
+               </div>
         </div>
-
+           <CarDetails />
     </div>
   )
 }
