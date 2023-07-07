f0841d13f8289fb78e3cd6834756568b9ff9977a
otienotilen <otienotilen@gmail.com>
2023-07-07 11:22:31 +0300

    Car Details Modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -19,9 +19,9 @@ const CarCard = ({ car }: CarCardProps) => {
 
   const carRent = calculateCarRent(city_mpg, year);
 
-    // function generateCarImageUrl(car: CarProps): string | ( | import("next/image").StaticImageData) {
-    //     throw new Error("Function not implemented.");
-    // }
+  // function generateCarImageUrl(car: CarProps): string | ( | import("next/image").StaticImageData) {
+  //     throw new Error("Function not implemented.");
+  // }
 
   return (
     <div className="car-card group">
@@ -31,21 +31,36 @@ const CarCard = ({ car }: CarCardProps) => {
         </h2>
       </div>
 
-      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
-        <span className='self-start text-[14px] leading-[17px] font-semibold'>$</span>
+      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
+        <span className="self-start text-[14px] leading-[17px] font-semibold">
+          $
+        </span>
         {carRent}
-        <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
-      </p> 
+        <span className="self-end text-[14px] leading-[17px] font-medium">
+          /day
+        </span>
+      </p>
 
-      <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
       </div>
 
-      <div className='relative flex w-full mt-2'>
-        <div className='flex group-hover:invisible w-full justify-between text-grey'>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
-            <p className='text-[14px] leading-[17px]'>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px] leading-[17px]">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
           </div>
@@ -61,16 +76,20 @@ const CarCard = ({ car }: CarCardProps) => {
 
         <div className="car-card__btn-container">
           <CustomButton
-            title='View More'
-            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-            textStyles='text-white text-[14px] leading-[17px] font-bold'
-            rightIcon='/right-arrow.svg'
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
             handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
 
-      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
