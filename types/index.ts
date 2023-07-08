1b597e9bfe49c59688a73d8832c842f253dece64
otienotilen <otienotilen@gmail.com>
2023-07-08 21:38:32 +0300

    server side rendering

--- types/index.ts
+++ types/index.ts
@@ -1,23 +1,21 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?:
-    MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit";
-    textStyles?: string;
-    rightIcon?: string;
-    isDisabled?: boolean;
-    
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
-export interface SearchManufacturerProps{
-    manufacturer: string;
-    setManufacturer: (manufacturer: string)=> void;
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
 
-export interface CarProps{
-    city_mpg: number;
+export interface CarProps {
+  city_mpg: number;
   class: string;
   combination_mpg: number;
   cylinders: number;
@@ -29,5 +27,11 @@ export interface CarProps{
   model: string;
   transmission: string;
   year: number;
-
+}
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
 }
--- utls/index.ts
+++ utls/index.ts
@@ -1,13 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
     "X-RapidAPI-Key": "6ce07d6bb8msh21bc1c189f8d247p1d2fc5jsn5a86065d39c4",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=
+    ${manufacturer}&year=${year}&model=${model}&limit=${limit}
+    &fuel_type=${fuel}`,
     {
       headers: headers,
     }
